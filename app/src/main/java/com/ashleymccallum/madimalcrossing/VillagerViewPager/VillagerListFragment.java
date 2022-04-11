ab949b851a8731160fdf0c72359dd5ebeb6962d1
Brooke Baird <w0766373@myscc.ca>
2022-04-11 15:03:02 -0400

    Remove the animation from the viewpager fragment

--- app/src/main/java/com/ashleymccallum/madimalcrossing/VillagerViewPager/VillagerListFragment.java
+++ app/src/main/java/com/ashleymccallum/madimalcrossing/VillagerViewPager/VillagerListFragment.java
@@ -1,31 +1,19 @@
 package com.ashleymccallum.madimalcrossing.VillagerViewPager;
 
-import static com.ashleymccallum.madimalcrossing.VillagerListRecycler.VillagerDetailHostActivity.ALL_VILLAGER_KEY;
-import static com.ashleymccallum.madimalcrossing.VillagerListRecycler.VillagerDetailHostActivity.LIST_ID;
-import static com.ashleymccallum.madimalcrossing.pojos.VillagerList.EDIT_KEY;
-
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-
-import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.ashleymccallum.madimalcrossing.AppDatabase;
+import androidx.fragment.app.Fragment;
+
 import com.ashleymccallum.madimalcrossing.R;
 import com.ashleymccallum.madimalcrossing.VillagerListRecycler.VillagerDetailHostActivity;
-import com.ashleymccallum.madimalcrossing.pojos.VillagerList;
 import com.google.android.material.snackbar.Snackbar;
-import com.royrodriguez.transitionbutton.TransitionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,7 +21,6 @@ import com.royrodriguez.transitionbutton.TransitionButton;
  * create an instance of this fragment.
  */
 public class VillagerListFragment extends Fragment {
-    private TransitionButton listButton;
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -77,45 +64,19 @@ public class VillagerListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_villager_viewpager, container, false);
         TextView listName = view.findViewById(R.id.listName);
-        listButton = view.findViewById(R.id.villagerListButton);
+        Button listButton = view.findViewById(R.id.villagerListButton);
 
         if(mParam1 != null && mParam2 != null) {
             listName.setText(mParam1);
             listButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    listButton.startAnimation();
-                    final Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            boolean isSuccessful = true;
-
-                            if (isSuccessful) {
-                                listButton.stopAnimation(TransitionButton.StopAnimationStyle.EXPAND, new TransitionButton.OnAnimationStopEndListener() {
-                                    @Override
-                                    public void onAnimationStopEnd() {
-                                        Intent i = new Intent(getActivity(), VillagerDetailHostActivity.class);
-                                        //TODO: button no longer loads correct list on click, always loads full villager list
-                                        i.putExtra(LIST_ID, mParam2);
-                                        i.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                                        try {
-                                            startActivity(i);
-                                        } catch (ActivityNotFoundException e) {
-                                            Snackbar.make(view, getString(R.string.ext_app_error), Snackbar.LENGTH_LONG).show();
-                                        }
-                                        listButton.clearAnimation();
-                                        listButton.setText(R.string.goto_villagers);
-
-
-                                    }
-                                });
-                            } else {
-                                listButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
-
-                            }
-                        }
-                    }, 2000);
+                    Intent i = new Intent(getActivity(), VillagerDetailHostActivity.class);
+                    try {
+                        startActivity(i);
+                    }catch (ActivityNotFoundException e) {
+                        Snackbar.make(view, "Could not open list", Snackbar.LENGTH_LONG).show();
+                    }
                 }
             });
         }
