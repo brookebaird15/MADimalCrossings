f6081213c059221041644573d13797113a7a7df0
Brooke Baird <w0766373@myscc.ca>
2022-04-15 22:29:17 -0400

    #1, #3 Add stroke to villager detail screen on: Info card, image card and house card

--- app/src/main/java/com/ashleymccallum/madimalcrossing/Bingo/BingoFragment.java
+++ app/src/main/java/com/ashleymccallum/madimalcrossing/Bingo/BingoFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +22,7 @@ import android.widget.TextView;
 
 import com.ashleymccallum.madimalcrossing.AppDatabase;
 import com.ashleymccallum.madimalcrossing.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
